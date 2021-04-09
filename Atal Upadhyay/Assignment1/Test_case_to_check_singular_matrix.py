30965776f41bfe97c64506d544f670002e7cbe26
atal98 <80337520+atal98@users.noreply.github.com>
2021-04-09 23:11:38 +0530

    Add files via upload

---
+++ Atal Upadhyay/Assignment1/Test_case_to_check_singular_matrix.py
@@ -0,0 +1,26 @@
+from check_singular_matrix import *
+import unittest
+
+class To_check_singular_matrix_Test_case(unittest.TestCase):
+    
+  #  def test_sigular_matrix_with_correct_values(self): # PASS
+  #      self.assertEqual(is_singular(matrix=[[2,4,6],[2,0,2],[6,8,14]]),0)
+  #      self.assertEqual(is_singular(matrix=[[0,0,0],[8,4,7],[9,7,10]]),0)
+  #      self.assertEqual(is_singular(matrix=[[2,3,5],[0,0,0],[9,15,4]]),0)
+
+  #  def test_sigular_matrix_with_incorrect_values(self): # FAILS
+  #      self.assertEqual(is_singular(matrix=[[1,1,1],[7,17,11],[19,13,1]]),0)
+  #      self.assertEqual(is_singular(matrix=[[11,23,20],[18,1,12],[5,7,3]]),0)
+  #      self.assertEqual(is_singular(matrix=[[12,8,6],[6,7,10],[3,8,14]]),0)
+
+# To Execute the Test case. 
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
---
+++ Atal Upadhyay/Assignment1/check_singular_matrix.py
@@ -0,0 +1,23 @@
+# a given matrix is a singular matrix or not in Python.
+#  A matrix is said to be a singular matrix if its determinant is equal to zero.
+
+#Tis function for to get cofactor of matrix[i][j].
+def cofactor(matrix, i, j):
+    return [row[: j] + row[j+1:] for row in (matrix[: i] + matrix[i+1:])]
+
+# This function is to check wheather matrix is singular or not.
+def is_singular(matrix):
+    n = len(matrix)  # n is the data 
+    if (n == 2):
+        val = matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1]   # Applying the Formula of determine in val 
+        return val
+
+# Declare a variable det to store the determinant.
+   
+    det = 0  # det means determinant 
+    # Apply the formula and store the result in det. Finally, return the determinant.
+    for i in range(n):
+        s = (-1)**i
+        sub_det = is_singular(cofactor(matrix, 0, i))   
+        det += (s*matrix[0][i]*sub_det)
+    return det
