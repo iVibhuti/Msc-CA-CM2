778a07d3355b82e8c455b4e0f082f206cd8c81b4
aradhay-sharma <81693331+aradhay-sharma@users.noreply.github.com>
2021-04-10 22:14:44 +0530

    Update singular_matrix.py

--- Aradhya/Assignment 1/singular_matrix.py
+++ Aradhya/Assignment 1/singular_matrix.py
@@ -1,36 +1,45 @@
 # Taking the input matrix from reading the file
-def InputMatrix(filename):
+def inputMatrix(filename):
     with open (filename, "r") as file:
-        data = []
+        matrix = []
         for row in file:
-            data.append([int(x) for x in row.split(",")])
+            matrix.append([int(x) for x in row.split(",")])
 
-    return data
+    return matrix
+
+
 #Function to get cofactor of matrix[i][j].
-def cofactor(matrix, x, y):
-    return [row[: y] + row[y+1:] for row in (matrix[: x] + matrix[x+1:])]
-
+def getcofactor(m, i, j):
+    return [row[: j] + row[j+1:] for row in (m[: i] + m[i+1:])]
+ 
 #Recursive function to check if matrix is singular or not.
-def check_for_singular(matrix):
-    n=len(matrix)
-    if (n== 2):
-        result = matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1]
-        return result
 
-    determinant = 0
-    for i in range(data):
-        val= (-1)**i
-        dt = check_for_singular(cofactor(matrix, 0, i))
-        determinant += (val*matrix[0][i]*dt)
-    return determinant
-# Main function
-if __name__ == '__main__':
-    input = InputMatrix("input.txt")
-    print("Printing the matrix: ")
-    print(input)
+def determinantOfMatrix(mat):
+ 
+    if(len(mat) == 2):
+        value = mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
+        return value
+ 
+    # initialize Sum to zero
+    Sum = 0
+ 
+    for current_column in range(len(mat)):
+ 
+        sign = (-1) ** (current_column)
+ 
+        sub_det = determinantOfMatrix(getcofactor(mat, 0, current_column))
+ 
+        Sum += (sign * mat[0][current_column] * sub_det)
+ 
+    # returning the final Sum
+    return Sum
 
-    n = 3
-    if (check_for_singular(matrix)):
-        print("Matrix is non-singular")
-    else:
-        print("Matrix is singular")
+
+# Taking the input matrix from reading the file
+matrix = inputMatrix("input.txt")
+determinant = determinantOfMatrix(matrix)
+
+if(determinant != 0):
+    print("Matrix is non-Singular.")
+else:
+    print("Matrix is Singular.")
