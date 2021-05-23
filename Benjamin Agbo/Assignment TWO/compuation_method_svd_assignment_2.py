8f5eb1ac51d574663ef6bdfa010d85b108bf5720
benjaminagbo <81688907+benjaminagbo@users.noreply.github.com>
2021-05-23 19:58:28 +0530

    Add files via upload

---
+++ Benjamin Agbo/Assignment TWO/compuation_method_svd_assignment_2.py
@@ -0,0 +1,80 @@
+import numpy as np
+from numpy.linalg import norm
+from random import normalvariate
+from math import sqrt
+
+
+def randomUnitVector(n):
+    unnormalized = [normalvariate(0, 1) for _ in range(n)]
+    theNorm = sqrt(sum(x * x for x in unnormalized))
+    return [x / theNorm for x in unnormalized]
+
+
+def svd_1d(A, epsilon=1e-10):
+    ''' The one-dimensional SVD '''
+
+    n, m = A.shape
+    x = randomUnitVector(min(n,m))
+    lastV = None
+    currentV = x
+
+    if n > m:
+        B = np.dot(A.T, A)
+    else:
+        B = np.dot(A, A.T)
+
+    iterations = 0
+    while True:
+        iterations += 1
+        lastV = currentV
+        currentV = np.dot(B, lastV)
+        currentV = currentV / norm(currentV)
+
+        if abs(np.dot(currentV, lastV)) > 1 - epsilon:
+            return currentV
+
+
+def svd(A, k=None, epsilon=1e-10):
+   
+#Factor of the given array  by Singular Value Decomposition
+  
+    A = np.array(A, dtype=float)
+    n, m = A.shape
+    svdSoFar = []
+    if k is None:
+        k = min(n, m)
+
+    for i in range(k):
+        matrixFor1D = A.copy()
+
+        for singularValue, u, v in svdSoFar[:i]:
+            matrixFor1D -= singularValue * np.outer(u, v)
+
+        if n > m:
+            v = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            u_unnormalized = np.dot(A, v)
+            sigma = norm(u_unnormalized)  # next singular value
+            u = u_unnormalized / sigma
+        else:
+            u = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            v_unnormalized = np.dot(A.T, u)
+            sigma = norm(v_unnormalized)  # next singular value
+            v = v_unnormalized / sigma
+
+        svdSoFar.append((sigma, u, v))
+
+    singularValues, us, vs = [np.array(x) for x in zip(*svdSoFar)]
+    print("U : ",us)
+    print("\nV : ",vs)
+    print("\nS : ",singularValues)
+    return singularValues, us.T, vs
+
+
+
+if __name__ == "__main__":
+    myarray = np.array([
+        [1, 2, 3],
+        [4, 6, 7],
+        [8, 9, 1]])
+    v1 = svd_1d(myarray)
+    theSVD = svd(myarray)
